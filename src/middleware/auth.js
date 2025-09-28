f4f82f629107603b1e3d9e80742b86eaf81e6e03
Maynor Alejandro Bolaños Orellana <1925597@clases.edu.sv>
2025-09-28 16:47:07 -0600

    fix plz

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,18 +3,23 @@ require('dotenv').config();
 
 const verifyToken = (req, res, next) => {
 
-    const token = req.header('Authorization');
-
-    if (!token) {
-        return res.status(401).json({ message: 'Acceso denegado. No se proporcionó un token' });
+    const bearerHeader = req.headers['authorization'];
+    
+    if (!bearerHeader) {
+        return res.status(403).json({ message: 'Se necesita token' });
     }
 
     try {
-        const decoded = jwt.verify(token.replace('Bearer ', ''), JWT_SECRET);
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        
+        // Verificar el token
+        const decoded = jwt.verify(bearerToken, process.env.JWT_SECRET);
+        
         req.user = decoded;
         next();
-    } catch (err) {
-        res.status(401).json({ message: 'Token inválido o expirado.' });
+    } catch (error) {
+        return res.status(401).json({ message: 'Token inválido' });
     }
      
 }
